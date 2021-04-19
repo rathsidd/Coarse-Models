64e9b158ee571da2ae0ea5d78af0573f2cf22e42
michaelm45 <52636486+michaelm45@users.noreply.github.com>
2021-04-19 10:50:15 -0700

    Delete AmoebotSim directory

--- AmoebotSim/helper/randomnumbergenerator.cpp
+++
@@ -1,7 +0,0 @@
-/* Copyright (C) 2020 Joshua J. Daymude, Robert Gmyr, and Kristian Hinnenthal.
- * The full GNU GPLv3 can be found in the LICENSE file, and the full copyright
- * notice can be found at the top of main/main.cpp. */
-
-#include "helper/randomnumbergenerator.h"
-
-std::mt19937 RandomNumberGenerator::rng;
